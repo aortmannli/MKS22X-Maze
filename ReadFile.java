0ffecbc828e6d64be62a5324b03766eaa4bd3aa4
Angel Ortmann-Li <ali02@stuy.edu>
2019-02-26 10:16:37 -0500

    did the printing thing for char 2d array

--- ReadFile.java
+++ ReadFile.java
@@ -6,10 +6,22 @@ public class ReadFile{
   public static void main(String[] args) throws FileNotFoundException {
     File f = new File(args[0]);
     Scanner in = new Scanner(f);
-
+    int height = 0;
+    int length = 0;
     while (in.hasNext()) {
       String out = in.nextLine();
+      height ++;
+      length = out.length();
       System.out.println(out);
+
     }
+    char[][] maze = new char[height][length];
+
+    for(int i = 0; i < maze.length; i++){
+      for(int x = 0; x < maze[0].length; x++){
+        System.out.print(maze[i][x]);
+      }
+    }
+
   }
 }
